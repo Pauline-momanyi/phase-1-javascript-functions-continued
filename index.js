c916f9b27a3c3c2e40e7ebae8175528c3faf7e2e
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-06 17:34:50 +0300

    Create functions and test there scope

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity){
+     return activity? `This Saturday, I want to ${activity}!`:"This Saturday, I want to roller-skate!"
+}
+const mondayWork = function (monActivity){
+    return monActivity? `This Monday, I will ${monActivity}.`:"This Monday, I will go to the office."
+}
+function wrapAdjective(wrap = '*'){
+    return function(wrap2 = "a hard worker"){
+        let emphatic = `You are ${wrap}${wrap2}${wrap}!`
+        return emphatic
+    }
+}
+wrapAdjective()()
